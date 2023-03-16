713a0bd9a2c3883a0a9a87057b9dfb1dd8598dfb
parsa.hassani30 <parsa.hassani30@gitlab.com>
2023-03-16 09:13:40 +0330

    refactor using new version

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import veProgress from "vue-ellipse-progress";
+import VueClipboard from 'vue3-clipboard'
 
-createApp(App).mount('#app')
+const app = createApp(App).use(veProgress);
+app.use(VueClipboard, {
+    autoSetContainer: true,
+    appendToBody: true,
+  })
+app.mount('#app')
