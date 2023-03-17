fbd71704d8e5adbac325574f861af02c5933933c
parsa.hassani30 <parsa.hassani30@gitlab.com>
2023-03-17 15:40:43 +0330

    web scanner

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import veProgress from "vue-ellipse-progress";
 import VueClipboard from 'vue3-clipboard'
 
 const app = createApp(App).use(veProgress);
+app.use(router)
+
 app.use(VueClipboard, {
     autoSetContainer: true,
     appendToBody: true,
